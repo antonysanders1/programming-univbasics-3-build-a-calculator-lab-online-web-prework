be452c7e1b3a224c75f2fa149f95c470d58ab9e2
Antony Sanders <antony.devon92@gmail.com>
2019-10-23 17:03:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,23 @@
 def addition(num1, num2)
-
+  num1.Float + num2.Float
 end
+  addition(3.0, 2.0)
 
 def subtraction(num1, num2)
-
+  num1.Float - num2.Float
 end
+  subtraction(42.0, 13.0)
 
 def division(num1, num2)
-
+  num1.Integer / num2.Integer
 end
+  division(81, 9)
 
 def multiplication(num1, num2)
-
+  num1.Integer * num2.Integer
 end
-
+  multiplication(2,2)
+  
 def modulo(num1, num2)
 
 end
